64b1975481f35ca0afe908760220947c9f2aa31f
Matthias Georg Imhof <mgimhof@yahoo.com>
2025-10-11 15:18:53 -0500

    finally add OpenCyphal customizations to project. Official OpenCyphal is submodule

---
+++ OpenCyphal/include/_4111Spyglass.h
@@ -0,0 +1,17 @@
+/*
+ * _4111Spyglass.h
+ *
+ *  Created on: Dec 5, 2024
+ *      Author: mgi
+ */
+
+#ifndef INC__4111SPYGLASS_H_
+#define INC__4111SPYGLASS_H_
+
+#define _4111spyglass_sat_sensor_Magnetometer_0_1_PORT_ID_  0x531
+#define _4111spyglass_sat_sensor_GNSS_0_1_PORT_ID_  0x533
+#define _4111spyglass_sat_data_SGP4_0_1_PORT_ID_  0x535
+#define _4111spyglass_sat_model_PositionVelocity_0_1_PORT_ID_  0x537
+#define _uavcan_si_sample_angle_Quaternion_1_0_PORT_ID_  0x539
+
+#endif /* INC__4111SPYGLASS_H_ */
