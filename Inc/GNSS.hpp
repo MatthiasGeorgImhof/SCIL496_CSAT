e39a453b275c1f4620b15ad56c4b91460b1ad54b
Matthias Georg Imhof <mgimhof@yahoo.com>
2025-10-16 19:11:26 -0500

    GNSS is using Transport

--- Inc/GNSS.hpp
+++ Inc/GNSS.hpp
@@ -33,6 +33,7 @@
 #include <optional>
 
 #include "GNSSCore.hpp"
+#include "Transport.hpp"
 
 #ifdef __arm__
 #include "usb_device.h"
@@ -89,11 +90,13 @@ enum UBXMessageID : uint8_t
 	VEL_NED = 0x12
 };
 
+template <typename Transport>
+	requires StreamTransport<Transport>
 class GNSS
 {
 public:
 	GNSS() = delete;
-	GNSS(UART_HandleTypeDef *huart);
+	GNSS(const Transport &transport) : transport_(transport) {}
 
 	void setMode(GNSSMode gnssMode);
 	std::optional<UniqueID> getUniqID();
@@ -114,7 +117,7 @@ private:
 	static constexpr uint16_t UBLOX_HEADER_SIZE = 6U;
 
 private:
-	UART_HandleTypeDef *huart;
+	const Transport &transport_;
 	uint8_t uart_buffer[GNSS_BUFFER_SIZE];
 
 };
@@ -133,5 +136,218 @@ private:
 private:
     int32_t error_meters;
 };
+
+#
+#
+#
+
+// https://content.u-blox.com/sites/default/files/u-blox-M10-SPG-5.10_InterfaceDescription_UBX-21035062.pdf
+
+/*!
+ * Searching for a header in data buffer and matching class and message ID to buffer data.
+ * @param GNSS Pointer to main GNSS structure.
+ */
+template <typename Transport>
+	requires StreamTransport<Transport>
+uint8_t *GNSS<Transport>::findHeader(UBXClass classID, UBXMessageID messageID)
+{
+	constexpr uint16_t SIZE = sizeof(uart_buffer) / 2;
+	for (uint16_t var = 0; var <= SIZE; ++var)
+	{
+		if (uart_buffer[var] == 0xB5 && uart_buffer[var + 1] == 0x62)
+		{
+			uint16_t length = GNSSCore::getUShort(uart_buffer, static_cast<uint16_t>(var + 4));
+			uint8_t cka_message = GNSSCore::getUByte(uart_buffer, static_cast<uint16_t>(var + length + UBLOX_HEADER_SIZE));
+			uint8_t ckb_message = GNSSCore::getUByte(uart_buffer, static_cast<uint16_t>(var + length + UBLOX_HEADER_SIZE + 1));
+			uint8_t cka, ckb;
+			GNSSCore::checksum(length + 4U, &uart_buffer[var + 2], &cka, &ckb);
+			if (cka != cka_message && ckb != ckb_message)
+			{
+				return nullptr;
+			}
+			if (uart_buffer[var + 2] == static_cast<uint8_t>(classID) && uart_buffer[var + 3] == static_cast<uint8_t>(messageID))
+			{
+				return uart_buffer + var + UBLOX_HEADER_SIZE;
+			}
+		}
+	}
+	return nullptr;
+}
+
+template <typename Transport>
+	requires StreamTransport<Transport>
+uint8_t *GNSS<Transport>::request(const uint8_t *request, uint16_t size, UBXClass classID, UBXMessageID messageID)
+{
+	transport_.send(const_cast<uint8_t *>(request), size);
+	transport_.receive(uart_buffer, sizeof(uart_buffer));
+	return findHeader(classID, messageID);
+}
+
+template <typename Transport>
+	requires StreamTransport<Transport>
+std::optional<UniqueID> GNSS<Transport>::getUniqID()
+{
+	uint8_t *messageBuffer = request(GNSSCore::GET_UNIQUE_ID, sizeof(GNSSCore::GET_UNIQUE_ID), UBXClass::MON, UBXMessageID::UNIQ_ID);
+	if (messageBuffer == nullptr)
+		return std::nullopt;
+	return GNSSCore::parseUniqID(messageBuffer);
+}
+
+/*!
+ * Make request for UTC time solution data.
+ */
+template <typename Transport>
+	requires StreamTransport<Transport>
+std::optional<UTCTime> GNSS<Transport>::getNavTimeUTC()
+{
+	uint8_t *messageBuffer = request(GNSSCore::GET_NAV_TIME_UTC, sizeof(GNSSCore::GET_NAV_TIME_UTC), UBXClass::NAV, UBXMessageID::UTC_TIME);
+	if (messageBuffer == nullptr)
+		return std::nullopt;
+	return GNSSCore::parseNavTimeUTC(messageBuffer);
+}
+
+/*!
+ * Make request for geodetic position solution data.
+ */
+template <typename Transport>
+	requires StreamTransport<Transport>
+std::optional<PositionLLH> GNSS<Transport>::getNavPosLLH()
+{
+	uint8_t *messageBuffer = request(GNSSCore::GET_NAV_POS_LLH, sizeof(GNSSCore::GET_NAV_POS_LLH), UBXClass::NAV, UBXMessageID::POS_LLH);
+	if (messageBuffer == nullptr)
+		return std::nullopt;
+	return GNSSCore::parseNavPosLLH(messageBuffer);
+}
+
+/*!
+ * Make request for earth centric position solution data.
+ */
+template <typename Transport>
+	requires StreamTransport<Transport>
+std::optional<PositionECEF> GNSS<Transport>::getNavPosECEF()
+{
+	uint8_t *messageBuffer = request(GNSSCore::GET_NAV_POS_ECEF, sizeof(GNSSCore::GET_NAV_POS_ECEF), UBXClass::NAV, UBXMessageID::POS_ECEF);
+	if (messageBuffer == nullptr)
+		return std::nullopt;
+	return GNSSCore::parseNavPosECEF(messageBuffer);
+}
+
+/*!
+ * Make request for navigation position velocity time solution data.
+ */
+template <typename Transport>
+	requires StreamTransport<Transport>
+std::optional<NavigationPVT> GNSS<Transport>::getNavPVT()
+{
+	uint8_t *messageBuffer = request(GNSSCore::GET_NAV_PVT, sizeof(GNSSCore::GET_NAV_PVT), UBXClass::NAV, UBXMessageID::PVT);
+	if (messageBuffer == nullptr)
+		return std::nullopt;
+	return GNSSCore::parseNavPVT(messageBuffer);
+}
+
+/*!
+ * Make request for geocentric navigation velocity solution data.
+ */
+template <typename Transport>
+	requires StreamTransport<Transport>
+std::optional<VelocityNED> GNSS<Transport>::getNavVelNED()
+{
+	uint8_t *messageBuffer = request(GNSSCore::GET_NAV_VEL_NED, sizeof(GNSSCore::GET_NAV_VEL_NED), UBXClass::NAV, UBXMessageID::VEL_NED);
+	if (messageBuffer == nullptr)
+		return std::nullopt;
+	return GNSSCore::parseNavVelNED(messageBuffer);
+}
+
+/*!
+ * Make request for earth centric navigation velocity solution data.
+ */
+template <typename Transport>
+	requires StreamTransport<Transport>
+std::optional<VelocityECEF> GNSS<Transport>::getNavVelECEF()
+{
+	uint8_t *messageBuffer = request(GNSSCore::GET_NAV_VEL_ECEF, sizeof(GNSSCore::GET_NAV_VEL_ECEF), UBXClass::NAV, UBXMessageID::VEL_ECEF);
+	if (messageBuffer == nullptr)
+		return std::nullopt;
+	return GNSSCore::parseNavVelECEF(messageBuffer);
+}
+
+/*!
+ * Changing the GNSS mode.
+ */
+template <typename Transport>
+	requires StreamTransport<Transport>
+void GNSS<Transport>::setMode(GNSSMode gnssMode)
+{
+	uint8_t *dataToSend = nullptr;
+	size_t dataSize = 0;
+
+	switch (gnssMode)
+	{
+	case GNSSMode::Portable:
+		dataToSend = const_cast<uint8_t *>(GNSSCore::SET_PORTABLE_MODE);
+		dataSize = sizeof(GNSSCore::SET_PORTABLE_MODE);
+		break;
+	case GNSSMode::Stationary:
+		dataToSend = const_cast<uint8_t *>(GNSSCore::SET_STATIONARY_MODE);
+		dataSize = sizeof(GNSSCore::SET_STATIONARY_MODE);
+		break;
+	case GNSSMode::Pedestrian:
+		dataToSend = const_cast<uint8_t *>(GNSSCore::SET_PEDESTRIAN_MODE);
+		dataSize = sizeof(GNSSCore::SET_PEDESTRIAN_MODE);
+		break;
+	case GNSSMode::Automotive:
+		dataToSend = const_cast<uint8_t *>(GNSSCore::SET_AUTOMOTIVE_MODE);
+		dataSize = sizeof(GNSSCore::SET_AUTOMOTIVE_MODE);
+		break;
+	case GNSSMode::Sea:
+		dataToSend = const_cast<uint8_t *>(GNSSCore::SET_SEA_MODE);
+		dataSize = sizeof(GNSSCore::SET_SEA_MODE);
+		break;
+	case GNSSMode::Airborne1G:
+		dataToSend = const_cast<uint8_t *>(GNSSCore::SET_AIRBORNE_1G_MODE);
+		dataSize = sizeof(GNSSCore::SET_AIRBORNE_1G_MODE);
+		break;
+	case GNSSMode::Airborne2G:
+		dataToSend = const_cast<uint8_t *>(GNSSCore::SET_AIRBORNE_2G_MODE);
+		dataSize = sizeof(GNSSCore::SET_AIRBORNE_2G_MODE);
+		break;
+	case GNSSMode::Airborne4G:
+		dataToSend = const_cast<uint8_t *>(GNSSCore::SET_AIRBORNE_4G_MODE);
+		dataSize = sizeof(GNSSCore::SET_AIRBORNE_4G_MODE);
+		break;
+	case GNSSMode::Wrist:
+		dataToSend = const_cast<uint8_t *>(GNSSCore::SET_WRIST_MODE);
+		dataSize = sizeof(GNSSCore::SET_WRIST_MODE);
+		break;
+	case GNSSMode::Bike:
+		dataToSend = const_cast<uint8_t *>(GNSSCore::SET_BIKE_MODE);
+		dataSize = sizeof(GNSSCore::SET_BIKE_MODE);
+		break;
+	default:
+		return;
+	}
+
+	if (dataToSend != nullptr)
+	{
+		transport_.send(dataToSend, static_cast<uint16_t>(dataSize));
+	}
+}
+
+/*!
+ *  Sends the basic configuration: Activation of the UBX standard, change of NMEA version to 4.10 and turn on of the Galileo system.
+ */
+template <typename Transport>
+	requires StreamTransport<Transport>
+void GNSS<Transport>::loadConfig()
+{
+	transport_.send(const_cast<uint8_t *>(GNSSCore::CONFIG_UBX), sizeof(GNSSCore::CONFIG_UBX));
+	HAL_Delay(250);
+	transport_.send(const_cast<uint8_t *>(GNSSCore::SET_NMEA_410), sizeof(GNSSCore::SET_NMEA_410));
+	HAL_Delay(250);
+	transport_.send(const_cast<uint8_t *>(GNSSCore::SET_GNSS), sizeof(GNSSCore::SET_GNSS));
+	HAL_Delay(250);
+}
+
+
 
 #endif /* INC_GNSS_H_ */
